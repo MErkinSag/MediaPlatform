9dec07541ec92d1e4028698e35ff618fef1b288d
erkin <erkin@apple-MacBook-Pro.local>
2022-12-27 17:13:48 +0300

    added comments for the db script

--- API/start_api.sh
+++ API/start_api.sh
@@ -6,13 +6,15 @@
 # This script checks if the container is started for the first time.
 
 CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
+
+# IF THIS IS THE FIRST STARTUP OF THE CONTAINER (i.e. The file was not created yet with the first docker run or docker compose up)
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
-    # place your script that you only want to run on first startup.
+
+    # create the tables on the database and fill them with dummy data, then start the fast api with uvicorn async worker
     python -m db.dummy.fill_db && uvicorn main:app --host 0.0.0.0 --port 8000
 
 else
-    # script that should run the rest of the times (instances where you 
-    # stop/restart containers).
+    # just start the fast api with uvicorn async worker
     uvicorn main:app --host 0.0.0.0 --port 8000
 fi
